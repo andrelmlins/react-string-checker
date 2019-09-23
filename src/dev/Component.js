92a1d25ce956c5408a96cb81ea13d1c997e8097a
André Lins <andrelucas01@hotmail.com>
2019-09-23 20:38:01 -0300

    Read elements

--- src/dev/Component.js
+++ src/dev/Component.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
 
 class AboutScreen extends React.Component {
   render() {
     return (
       <Typography variant="h2" color="inherit">
+        <h2>teste</h2>
         About us
       </Typography>
     );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-const babel = require('@babel/core');
-const compiler = require('./compiler');
+const babel = require("@babel/core");
+const compiler = require("./compiler");
 
 const babelConfigs = {
   ast: true,
-  presets: ['@babel/preset-react']
+  presets: ["@babel/preset-react"]
 };
 
-babel.transformFile('src/dev/Component.js', babelConfigs, function(err, result) {
+babel.transformFile("src/dev/Component.js", babelConfigs, (err, result) => {
   const body = result.ast.program.body;
-  compiler.run(body);
+  const listElements = readBody(result.ast.program);
+
+  listElements.map(item => {
+    if (item.type === "StringLiteral") {
+      console.log(item);
+    }
+  });
 });
+
+const readBody = element => {
+  console.log(element.type);
+  let elements = [];
+  if (element.body) {
+    if (element.body.length > 0) {
+      element.body.map(item => readBody(item).map(item => elements.push(item)));
+    } else {
+      readBody(element.body).map(item => elements.push(item));
+    }
+  }
+
+  if (element.argument) {
+    element.argument.arguments.map(item =>
+      readBody(item).map(item => elements.push(item))
+    );
+  }
+  return elements;
+};
