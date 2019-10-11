2e0c4af059a9171ef283ce15143ad78b34386c60
André Lins <andrelucas01@hotmail.com>
2019-10-11 08:02:04 -0300

    Insert core to NodeJS

---
+++ index.js
@@ -0,0 +1,23 @@
+const readAllFiles = require("./utils/readAllFiles");
+const listStrings = require("./compiler/listStrings");
+
+class ReactStringChecker {
+  runChecker(path) {
+    const files = readAllFiles(path);
+
+    files.map(file => {
+      if (file.match("^.+\\.(js|jsx|ts|tsx)$")) {
+        const strings = listStrings(file);
+
+        if (strings.length > 0) {
+          console.log("\x1b[33m", `File ${file}`, "\x1b[0m");
+          strings.map(string =>
+            console.log("\x1b[31m", `\tString: "${string.value}"`, "\x1b[0m")
+          );
+        }
+      }
+    });
+  }
+}
+
+module.exports = ReactStringChecker;
