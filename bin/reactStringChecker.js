2bf79fb0c28251b54b6fd70a27433cb525408406
André Lins <andrelucas01@hotmail.com>
2019-10-14 20:56:52 -0300

    Insert command option path

--- bin/reactStringChecker.js
+++ bin/reactStringChecker.js
@@ -4,6 +4,22 @@
 
 const CheckerStrings = require("../index.js");
 
-const checkerStrings = new CheckerStrings("./src");
+const args = process.argv.slice(2);
+let path = "./src";
+
+if (args.includes("--path") || args.includes("-p")) {
+  const position = args.includes("--path")
+    ? args.indexOf("--path")
+    : args.indexOf("-p");
+
+  if (position + 1 < args.length) {
+    path = args[position + 1];
+  } else {
+    console.log("Path not defined");
+    process.exit(1);
+  }
+}
+
+const checkerStrings = new CheckerStrings(path);
 
 checkerStrings.run();
